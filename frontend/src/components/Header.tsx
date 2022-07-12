e7fdf39b420c920f8256539a43c4d1b217c5deb3
Choejeonghun <saa00123@naver.com>
2022-07-12 17:27:12 +0900

    style: style 적용

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 
-
+const Logo = styled.div`
+    padding:30px;
+`
 
 export default function Header() {
     return (    
-        <div className="Header">
-            <div className="Logo">
+        <Logo>
+            <div className="Header">
+                <div className="Logo">
                     <img className="logoImage" alt="logo" src="images/logo.png"/>
-                <img className="paletteImage" alt="palette" src="images/palette.png"/>
+                    <img className="paletteImage" alt="palette" src="images/palette.png"/>
+                </div>
             </div>
-        </div>
+        </Logo>
     );
 }
