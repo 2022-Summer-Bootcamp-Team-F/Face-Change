727fb6eefb41d43e4ef79ea27c4c01e54cfaf716
Choejeonghun <saa00123@naver.com>
2022-07-12 17:22:03 +0900

    fix: 오류 수정 1차

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import styles from './Header.module.css';
+
+
 
 export default function Header() {
-    return (
-        <div className={styles.Header}>
+    return (    
+        <div className="Header">
             <div className="Logo">
-                <img className="logoImage" alt="logo" src="images/logo.png"/>
+                    <img className="logoImage" alt="logo" src="images/logo.png"/>
                 <img className="paletteImage" alt="palette" src="images/palette.png"/>
             </div>
         </div>
