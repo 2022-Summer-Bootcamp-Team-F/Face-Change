7406002f4f57c8e7534c6ac17146126bbcb4f659
Choejeonghun <saa00123@naver.com>
2022-07-12 17:58:35 +0900

    style: style 수정

--- frontend/src/components/PicturePageBtn.tsx
+++ frontend/src/components/PicturePageBtn.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
-import styles from './PicturePageBtn.module.css';
+import styled from 'styled-components';
+
+const Btn = styled.button`width: 18.063rem;
+    height: 4.25rem;
+    padding: 0.625rem 4rem 0.625rem 4.188rem;
+    border-radius: 30px;
+    background-color: #4354f5;
+
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const Btntag = styled.p`
+    width: 9.875rem;
+    height: 3rem;
+    font-family: Inter;
+    font-size: 2.5rem;
+    font-weight: 500;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: normal;
+    text-align: left;
+    color: #fff;
+`
 
 export default function PicturePageBtn() {
     return (
-        <div className={styles.PicturePageBtn}>
-            <button className={styles.btn}>
-                <p className={styles.choice}>사진 선택</p>
-            </button>
-        </div>
+        <Btn>
+            <Btntag>
+                <p className="Btntag">사진 선택</p>
+            </Btntag>
+        </Btn>
     );
 }
