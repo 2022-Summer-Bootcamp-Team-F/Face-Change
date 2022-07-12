8d6e9e0a06ffde5037424b5d9498909eafd1890c
Choejeonghun <saa00123@naver.com>
2022-07-12 17:54:49 +0900

    style: style 수정

--- frontend/src/components/ExamplePictures.tsx
+++ frontend/src/components/ExamplePictures.tsx
@@ -1,21 +1,56 @@
 import React from 'react';
-import styles from './ExamplePictures.module.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    display: flex;
+`
+
+const Cartoon = styled.section`
+    width: 18.75rem;
+    height: 18.75rem;
+    margin: 3.938rem 7.688rem 0.875rem 5.125rem;
+    padding: 4.563rem 2.5rem 1.063rem 3.125rem;
+    object-fit: contain;
+    border-radius: 100px;
+    text-align: center;
+    font-size: 3rem;
+`
+const Caricature = styled.section`
+    width: 18.75rem;
+    height: 18.75rem;
+    margin: 3.938rem 7.688rem 0.875rem 5.125rem;
+    padding: 4.563rem 2.5rem 1.063rem 3.125rem;
+    object-fit: contain;
+    border-radius: 100px;
+    text-align: center;
+    font-size: 3rem;
+`
+const Anime = styled.section`
+    width: 18.75rem;
+    height: 18.75rem;
+    margin: 3.938rem 7.688rem 0.875rem 5.125rem;
+    padding: 4.563rem 2.5rem 1.063rem 3.125rem;
+    object-fit: contain;
+    border-radius: 100px;
+    text-align: center;
+    font-size: 3rem;
+`
 
 export default function ExamplePictures() {
     return (
-        <div className={styles.ExamplePictures}>
-            <div className={styles.cartoon}>
+        <Container>
+            <Cartoon>
                 <img className="Cartoon" alt="cartoon" src="images/cartoon.png"/>
                 <p>cartoon</p>
-            </div>
-            <div className={styles.caricature}>
+            </Cartoon>
+            <Caricature>
                 <img className="Caricature" alt="caricature" src="images/caricature.png"/>
                 <p>caricature</p>
-            </div>
-            <div className={styles.anime}>
+            </Caricature>
+            <Anime>
                 <img className="anime" alt="anime" src="images/anime.png"/>
                 <p>anime</p>
-            </div>
-        </div>
+            </Anime>
+        </Container>
     );
 }
