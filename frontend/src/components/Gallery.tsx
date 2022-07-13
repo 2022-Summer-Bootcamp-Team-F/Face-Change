296c5002a93e0d66c66911a668a50dc47b06b8b6
Dong Seunghyeon <kj94gakt@naver.com>
2022-07-14 00:59:28 +0900

    style: gallery (Man) style 적용

--- frontend/src/components/Gallery.tsx
+++ frontend/src/components/Gallery.tsx
@@ -1,5 +1,19 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Man = styled.section`
+  width: 18.75rem;
+  height: 18.75rem;
+  margin: 3.938rem 7.688rem 0.875rem 5.125rem;
+  padding: 4.563rem 2.5rem 1.063rem 3.125rem;
+  object-fit: contain;
+  border-radius: 100px;
+`;
 
 export default function Gallery() {
-    return <div>Gallery</div>;
+  return <div>Gallery</div>;
 }
