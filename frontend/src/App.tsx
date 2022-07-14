d5538d032f8e8d2e44a46490d2042adce75e405d
Choejeonghun <saa00123@naver.com>
2022-07-14 17:31:14 +0900

    feat: background-image 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from "styled-components";
 
 import Header from './components/Header';
 import Comment from './components/Comment';
@@ -7,16 +8,24 @@ import ExamplePictures from './components/ExamplePictures';
 import Scroll from './components/Scroll';
 import Gallery from './components/Gallery';
 
+const BackgroundImage = styled.div`
+  background-image: url('images/background.png');
+  width:100vw;
+  height:100vh;
+  background-repeat: no-repeat;
+  background-size: cover;
+`
+
 function App() {
   return (
-    <div>
+    <BackgroundImage>
       <Header />
       <Comment />
       <PicturePageBtn />
       <ExamplePictures />
       <Scroll />
       <Gallery />
-    </div>
+    </BackgroundImage>
   );
 }
 
