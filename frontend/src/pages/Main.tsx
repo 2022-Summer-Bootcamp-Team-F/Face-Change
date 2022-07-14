dadaf2fc56a62ddfdc3656405c79a98fc1d547a6
Choejeonghun <saa00123@naver.com>
2022-07-14 19:05:57 +0900

    feat: 기존 mainpage UI 이동 완료

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,9 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+
+import Header from '../components/Header';
+import Comment from '../components/Comment';
+import PicturePageBtn from '../components/PicturePageBtn';
+import ExamplePictures from '../components/ExamplePictures';
+import Scroll from '../components/Scroll';
+import Gallery from '../components/Gallery';
+
+const BackgroundImage = styled.div`
+  background-image: url('images/background.png');
+  width:100vw;
+  height:100vh;
+  background-repeat: no-repeat;
+  background-size: cover;
+`
 
 export default function Main() {
     return(
-        <div>
-            <h1>Main</h1>
-        </div>
+        <BackgroundImage>
+            <Header />
+            <Comment />
+            <PicturePageBtn />
+            <ExamplePictures />
+            <Scroll />
+            <Gallery />
+      </BackgroundImage>
     );
 }
