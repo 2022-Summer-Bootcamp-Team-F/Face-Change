12be1d7b6456be3ddb1f4c53f62adb2547bd5685
Choejeonghun <saa00123@naver.com>
2022-07-14 18:36:19 +0900

    feat: background-image 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
 
 import Header from './components/Header';
 import Comment from './components/Comment';
