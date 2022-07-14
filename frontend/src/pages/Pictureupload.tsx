b070bb6462031cfcb6fe22a1d528bad4bbc0504c
Choejeonghun <saa00123@naver.com>
2022-07-14 18:51:05 +0900

    chore: routing page 생성

---
+++ frontend/src/pages/Pictureupload.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Pictureupload() {
+    return(
+        <div>
+            <h1>Pictureupload</h1>
+        </div>
+    );
+}
---
+++ frontend/src/pages/Roading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Roading() {
+    return(
+        <div>
+            <h1>Roading</h1>
+        </div>
+    );
+}
