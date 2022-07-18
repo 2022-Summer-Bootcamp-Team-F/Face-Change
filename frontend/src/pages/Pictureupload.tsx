8bc710bc893fa1e0f81cd44afffba3c462a53510
Choejeonghun <saa00123@naver.com>
2022-07-18 14:20:39 +0900

    style: 코드 간소화 및 스타일 적용

--- frontend/src/pages/Pictureupload.tsx
+++ frontend/src/pages/Pictureupload.tsx
@@ -1,5 +1,36 @@
 /* eslint-disable react/button-has-type */
 import React, {useState} from "react";
+import styled from "styled-components";
+
+const Btn = styled.button`
+    background-color: #9a9a9a;
+    width: 16.875rem;
+    height: 4rem;
+    border-radius: 50px;
+    cursor: pointer;
+
+    margin: 0 auto;
+    padding: 1.063rem 5.375rem 1.063rem 5.313rem;
+    
+    font-family: Inter;
+    font-size: 1.563rem;
+    font-weight: 500;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: normal;
+    text-align: left;
+    color: #fff;
+`;
+
+const Container = styled.div`
+    align-Items: center;
+    justify-Content: center;
+`
+
+const Img = styled.img`
+    margin: auto;
+`
 
 function Pictureupload() {
     const [fileImage, setFileImage] = useState("");
@@ -14,50 +45,25 @@ function Pictureupload() {
     };
 
     return (
-        <>
-        <h1>이미지 미리보기</h1>
-        <table>
-            <tr>
-                <th>이미지</th>
-                <td>
-                    <div>
-                        {fileImage && (
-                            <img
-                            alt = "sample"
-                            src={fileImage}
-                            style={{margin: "auto"}}
-                            />
-                        )}
-                        <div
-                        style={{
-                            alignItems: "center",
-                            justifyContent: "center",
-                        }}
-                        >
-                            <label htmlFor="image">
-                            <input
-                            name="imgUpload"
-                            type="file"
-                            accept="image/*"
-                            onChange={saveFileImage}
-                            />
-                            <button
-                            style={{
-                                backgroundColor: "gray",
-                                color: "white",
-                                width: "55px",
-                                height: "40px",
-                                cursor: "pointer",
-                            }}
-                            onClick={() => deleteFileImage()}
-                            >삭제</button>
-                            </label>
-                        </div>
-                    </div>
-                </td>
-            </tr>
-        </table>
-        </>
+        <div>
+            <div>
+                {fileImage && (
+                    <Img
+                    alt = "sample"
+                    src={fileImage} />
+                )}
+                <Container>
+                    <label htmlFor="image">
+                    <input
+                    name="imgUpload"
+                    type="file"
+                    accept="image/*"
+                    onChange={saveFileImage} />
+                    <Btn onClick={() => deleteFileImage()}>삭제</Btn>
+                    </label>
+                </Container>
+            </div>
+        </div>
     );
 }
 
