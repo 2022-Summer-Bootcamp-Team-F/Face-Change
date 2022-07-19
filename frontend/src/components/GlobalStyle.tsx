a4b37b8f3a54a364ffe1e84ec742eeedc0f47443
Choejeonghun <saa00123@naver.com>
2022-07-19 15:30:00 +0900

    feat: 배경 및 로고를 글로벌스타일에 생성

---
+++ frontend/src/components/GlobalStyle.tsx
@@ -0,0 +1,11 @@
+import {createGlobalStyle} from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    background-image: url('images/background.png');
+    width:100vw;
+    height:100vh;
+    background-repeat: no-repeat;
+    background-size: cover;
+`
+
+export default GlobalStyle;
