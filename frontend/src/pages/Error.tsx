d7c7c96dd05615eb479d32255141652740ce63f8
Dong Seunghyeon <kj94gakt@naver.com>
2022-07-19 14:59:20 +0900

    style: ErrorPage style Error Back Img 적용

--- frontend/src/pages/Error.tsx
+++ frontend/src/pages/Error.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const ErrorBackImg = styled.div`
+  background-image: url('images/ErrorBackImg.png');
+  width: 100vw;
+  height: 100vh;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
 
 export default function Error() {
-    return(
-        <div>
-            <h1>Error</h1>
-        </div>
-    );
+  return (
+    <ErrorBackImg>
+      <div>error</div>
+    </ErrorBackImg>
+  );
 }
