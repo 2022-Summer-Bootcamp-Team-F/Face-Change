436415eb9f13ae7c9391f134cfae3140c3154b96
Dong Seunghyeon <kj94gakt@naver.com>
2022-07-19 13:57:02 +0900

    style: gallery page img style 수정

--- frontend/src/components/Gallery.tsx
+++ frontend/src/components/Gallery.tsx
@@ -7,9 +7,9 @@ const Container = styled.div`
 `;
 
 const BMan = styled.section`
-  width: 24rem;
-  height: 23rem;
-  border: 6px solid;
+  width: 20rem;
+  height: 19rem;
+  /* border: 3px solid; */
   margin: 0 auto;
   padding: 2rem 2.5rem 1rem 2rem;
   object-fit: contain;
@@ -19,9 +19,9 @@ const BMan = styled.section`
 `;
 
 const BMan2 = styled.section`
-  width: 24rem;
-  height: 23rem;
-  border: 6px solid;
+  width: 20rem;
+  height: 19rem;
+  /* border: 3px solid; */
   margin: 0 auto;
   padding: 2rem 2.5rem 1rem 2rem;
   object-fit: contain;
@@ -30,97 +30,52 @@ const BMan2 = styled.section`
   font-size: 3rem;
 `;
 
-const BMan3 = styled.section`
-  width: 24rem;
-  height: 23rem;
-  border: 6px solid;
-  margin: 0 auto;
-  padding: 2rem 2.5rem 1rem 2rem;
-  object-fit: contain;
-  border-radius: 50px;
-  text-align: center;
-  font-size: 3rem;
-`;
-
-const BMan4 = styled.section`
-  width: 24rem;
-  height: 23rem;
-  border: 6px solid;
-  margin: 84px;
-  padding: 2rem 2.5rem 1rem 2rem;
-  object-fit: contain;
-  border-radius: 50px;
-  text-align: center;
-  font-size: 3rem;
-`;
-
-const BMan5 = styled.section`
-  width: 24rem;
-  height: 23rem;
-  border: 6px solid;
-  margin: 84px;
-  padding: 2rem 2.5rem 1rem 2rem;
-  object-fit: contain;
-  border-radius: 50px;
-  text-align: center;
-  font-size: 3rem;
-`;
-
-const BMan6 = styled.section`
-  width: 24rem;
-  height: 23rem;
-  border: 6px solid;
-  margin: 84px;
-  padding: 2rem 2.5rem 1rem 2rem;
-  object-fit: contain;
-  border-radius: 50px;
-  text-align: center;
-  font-size: 3rem;
+const Img = styled.img`
+  width: 19rem;
+  height: 18rem;
 `;
 
 export default function Gallery() {
   return (
     <Container>
       <BMan>
-        <img className="BMan" alt="bman" src="images/gallery_images/BMan.jpg" />
+        <Img className="BMan" alt="bman" src="images/gallery_images/BMan.jpg" />
       </BMan>
-      <BMan2>
-        <img
+      <BMan>
+        <Img
           className="BMan2"
           alt="bman"
           src="images/gallery_images/BMan2.jpg"
         />
-      </BMan2>
-      <BMan3>
-        <img
+      </BMan>
+      <BMan>
+        <Img
           className="BMan3"
           alt="bman"
           src="images/gallery_images/BMan3.jpg"
         />
-      </BMan3>
-      <Container>
-        <BMan4>
-          <img
-            className="BMan4"
-            alt="bman"
-            src="images/gallery_images/BMan4.jpg"
-          />
-        </BMan4>
-        <BMan5>
-          <img
-            className="BMan4"
-            alt="bman"
-            src="images/gallery_images/BMan5.jpg"
-          />
-        </BMan5>
-        <BMan6>
-          <img
-            className="BMan4"
-            alt="bman"
-            src="images/gallery_images/BMan6.jpg"
-          />
-        </BMan6>
-      </Container>
+      </BMan>
+      <BMan2>
+        <Img
+          className="BMan4"
+          alt="bman"
+          src="images/gallery_images/BMan4.jpg"
+        />
+      </BMan2>
+      <BMan2>
+        <Img
+          className="BMan4"
+          alt="bman"
+          src="images/gallery_images/BMan5.jpg"
+        />
+      </BMan2>
+      <BMan2>
+        <Img
+          className="BMan4"
+          alt="bman"
+          src="images/gallery_images/BMan6.jpg"
+        />
+      </BMan2>
     </Container>
   );
 }
