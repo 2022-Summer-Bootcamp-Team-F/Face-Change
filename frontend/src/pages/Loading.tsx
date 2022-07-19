9f658b034fde6a8609bb44701d7db37b43a950f7
Choejeonghun <saa00123@naver.com>
2022-07-19 15:12:34 +0900

    feat: 로딩 페이지 UI 완료

--- frontend/src/pages/Loading.tsx
+++ frontend/src/pages/Loading.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 const LoadingMent = styled.div`
     width: 45.313rem;
     height: 3.625rem;
-    margin: 5.438rem 0 1.625rem 19.688rem;
+    margin: 0 auto;
     font-family: Inter;
     font-size: 3rem;
     font-weight: 500;
@@ -12,15 +12,35 @@ const LoadingMent = styled.div`
     font-style: normal;
     line-height: normal;
     letter-spacing: normal;
-    text-align: left;
+    text-align: center;
     color: #000;
+
+    align-items: center;
+    justify-content: center;
+    margin-top: 10rem;
+    display: flex;
 `
 
-export default function Roading() {
+const Img = styled.img`
+    width: 12rem;
+    height: 13rem;
+    margin: 0 auto;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+    margin-top: 15rem;
+`
+
+
+export default function Loading() {
     return(
         <div>
+            <Img
+            alt="spinner"
+            src="images/spinner.png"/>
             <LoadingMent>
                 당신을 위한 만화를 그리고 있습니다.
+                잠시만 기다려 주세요...
             </LoadingMent>
         </div>
     );
