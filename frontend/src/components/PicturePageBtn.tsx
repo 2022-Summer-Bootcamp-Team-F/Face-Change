5b75088d752059e20f558b22787bc0f4e91c0902
Choejeonghun <saa00123@naver.com>
2022-07-19 22:19:43 +0900

    style: 코드 간소화

--- frontend/src/components/PicturePageBtn.tsx
+++ frontend/src/components/PicturePageBtn.tsx
@@ -57,9 +57,6 @@ export default function PicturePageBtn() {
           <path d="M50,250c0-110.5,89.5-200,200-200s200,89.5,200,200s-89.5,200-200,200S50,360.5,50,250" id="textcircle_top">
             <animateTransform attributeName="transform" begin="0s" dur="20s" type="rotate" from="0 250 250" to="360 250 250" repeatCount="indefinite" />
           </path>
-          <path d="M50,250c0-110.5,89.5-200,200-200s200,89.5,200,200s-89.5,200-200,200S50,360.5,50,250" id="textcircle_bottom">
-            <animateTransform attributeName="transform" begin="0s" dur="20s" type="rotate" from="195 250 250" to="555 250 250" repeatCount="indefinite" />
-          </path>
         </defs>
           <text dy="90" textLength="1220">
             <textPath xlinkHref="#textcircle_top">사진 선택 + 사진 선택 + 사진 선택 + 사진 선택 + </textPath>
