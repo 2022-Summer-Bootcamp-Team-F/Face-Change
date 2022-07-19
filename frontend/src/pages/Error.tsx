e9486faa4d2f58e81023bbee3761e071332d956a
Dong Seunghyeon <kj94gakt@naver.com>
2022-07-19 15:01:07 +0900

    style: ErrorPage style Error Icon 적용

--- frontend/src/pages/Error.tsx
+++ frontend/src/pages/Error.tsx
@@ -10,9 +10,25 @@ const ErrorBackImg = styled.div`
   background-size: cover;
 `;
 
+const ErrorIcon = styled.div`
+  width: 15rem;
+  height: 15rem;
+  margin: 0 auto;
+  padding: 4.563rem 2.5rem 1.063rem 3.125rem;
+  object-fit: contain;
+  border-radius: 100px;
+  text-align: center;
+  font-size: 3rem;
+
+  position: relative;
+`;
+
 export default function Error() {
   return (
     <ErrorBackImg>
+      <ErrorIcon>
+        <img className="Cartoon" alt="cartoon" src="images/ErrorIcon.png" />
+      </ErrorIcon>
       <div>error</div>
     </ErrorBackImg>
   );
