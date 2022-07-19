b7d2f70c378f771b8a1444a658bdc92bf735dfc2
Choejeonghun <saa00123@naver.com>
2022-07-19 14:13:00 +0900

    style: 변수명 변경에 의한 코드 수정

--- frontend/src/pages/Loading.tsx
+++ frontend/src/pages/Loading.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import styled from "styled-components";
 
+const LoadingMent = styled.div`
+    width: 45.313rem;
+    height: 3.625rem;
+    margin: 5.438rem 0 1.625rem 19.688rem;
+    font-family: Inter;
+    font-size: 3rem;
+    font-weight: 500;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: normal;
+    text-align: left;
+    color: #000;
+`
+
 export default function Roading() {
     return(
         <div>
-            <h1>Roading</h1>
+            <LoadingMent>
+                당신을 위한 만화를 그리고 있습니다.
+            </LoadingMent>
         </div>
     );
 }
