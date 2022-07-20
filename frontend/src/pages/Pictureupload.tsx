696ad8d506d2e9f74c828387e476e675f7543f49
Choejeonghun <saa00123@naver.com>
2022-07-20 12:18:12 +0900

    chore: 테스트 코드 삭제

--- frontend/src/pages/Pictureupload.tsx
+++ frontend/src/pages/Pictureupload.tsx
@@ -133,7 +133,6 @@ function Pictureupload() {
                 src={fileImage} />
             )}
             <BtnContainer>
-                <h1 className="text-3xl font-bold underline">Hello World!</h1>
                 <Label htmlFor="name">
                     사진 선택
                 <InputFile
