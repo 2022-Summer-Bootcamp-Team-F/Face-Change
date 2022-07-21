c20e6af1c1d1ce4a141bdeee5458d664b3d05a63
Choejeonghun <saa00123@naver.com>
2022-07-21 16:45:34 +0900

    feat: 출력된 결과 이미지 클릭 시 중앙에 이미지 출력

--- frontend/src/pages/Results.tsx
+++ frontend/src/pages/Results.tsx
@@ -1,26 +1,49 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable react/jsx-no-useless-fragment */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable react/button-has-type */
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import BackgroundImage from "../components/BackgroundImage";
 
+const list = [
+  { id: "1", key: "1", img: "images/palette.png" },
+  { id: "2", key: "2", img: "images/caricature2.png" },
+  { id: "3", key: "3", img: "images/logo.png" },
+  { id: "4", key: "4", img: "images/slider.png" },
+  { id: "5", key: "5", img: "images/anime2.png" },
+  { id: "6", key: "6", img: "images/cartoon2.png" },
+  { id: "7", key: "7", img: "images/background.png" },
+  { id: "8", key: "8", img: "images/Spinner.png" },
+];
+
 export default function Results() {
+  const [image, setImage] = useState("");
+  const listImage = list.map(({ key, img }) => (
+    <li>
+      <img
+        alt=""
+        src={img}
+        onClick={() => {
+          setImage(key);
+        }}
+      />
+    </li>
+  ));
+  const resultImage = list.map(({ key, img }) => (
+    <div>{image === key ? <img alt="" src={img} key={key} /> : null}</div>
+  ));
   return (
     <BackgroundImage>
       <Header />
       <div className="container flex justify-center items-center m-auto">
         <div className="overflow-auto h-80 w-12">
-          <img className="mt-2" alt="" src="images/palette.png" />
-          <img className="mt-2" alt="" src="images/caricature2.png" />
-          <img className="mt-2" alt="" src="images/logo.png" />
-          <img className="mt-2" alt="" src="images/slider.png" />
-          <img className="mt-2" alt="" src="images/anime2.png" />
-          <img className="mt-2" alt="" src="images/cartoon2.png" />
-          <img className="mt-2" alt="" src="images/background.png" />
-          <img className="mt-2" alt="" src="images/Spinner.png" />
+          <ul>{listImage}</ul>
         </div>
-        <div className="flex justify-center items-center border-dotted h-80 w-80 p-4 border-4 ml-3 ...">
-          <p>결과 이미지</p>
+        <div className="flex justify-center items-center border-dotted h-80 w-80 p-4 border-4 ml-3">
+          {resultImage}
         </div>
       </div>
       <div className="Container flex justify-center items-center mt-20">
