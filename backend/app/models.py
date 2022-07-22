1ffbe529040ca2f65a828a68769334eea8413a55
Junyoung Choi <pantomkid@naver.com>
2022-07-22 21:21:07 +0900

    chore: input collection 생성

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,8 @@
+from pyexpat import model
 from django.db import models
+from django.utils.timezone import datetime
 
-# Create your models here.
+class Input_Image_URL(models.Model):
+    pub_date = models.DateTimeField('date published', default=datetime.now, editable=False)
+    image_url = models.CharField(max_length=100)
+    
