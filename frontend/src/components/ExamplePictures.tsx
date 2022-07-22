707c1802305a7c7d638b29c9d83f29b37b5f64f4
Dong Seunghyeon <kj94gakt@naver.com>
2022-07-22 21:37:17 +0900

    style: MainRemodel style 수정

--- frontend/src/components/ExamplePictures.tsx
+++ frontend/src/components/ExamplePictures.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import 'react-responsive-carousel/lib/styles/carousel.css';
-// import '../carousel copy.css';
+import './carousel2.css';
 import { Carousel } from 'react-responsive-carousel';
 
 const ChangeImage = styled.div`
@@ -18,23 +18,43 @@ const ChangeImage = styled.div`
 
 export default function ExamplePictures() {
   return (
-    <Carousel showArrows width="30rem" showThumbs={false} infiniteLoop>
-      <ChangeImage>
-        <p>cartoon</p>
-        <img className="Cartoon" alt="cartoon" src="images/anime2.png" />
-      </ChangeImage>
-      <ChangeImage>
-        <p>caricature</p>
-        <img
-          className="Caricature"
-          alt="caricature"
-          src="images/caricature2.png"
-        />
-      </ChangeImage>
-      <ChangeImage>
-        <p>anime</p>
-        <img className="anime" alt="anime" src="images/cartoon2.png" />
-      </ChangeImage>
-    </Carousel>
+    <div className="flex float-right mr-80">
+      <Carousel
+        showArrows
+        width="30rem"
+        showThumbs={false}
+        infiniteLoop
+        autoPlay
+      >
+        <ChangeImage>
+          <p>cartoon</p>
+          <img className="Cartoon" alt="cartoon" src="images/All.jpg" />
+        </ChangeImage>
+        <ChangeImage>
+          <p>caricature</p>
+          <img
+            className="Caricature"
+            alt="caricature"
+            src="images/girlAll.jpg"
+          />
+        </ChangeImage>
+        <ChangeImage>
+          <p>anime</p>
+          <img className="anime" alt="anime" src="images/girlAll2.jpg" />
+        </ChangeImage>
+        <ChangeImage>
+          <p>anime</p>
+          <img className="anime" alt="anime" src="images/girlAll3.jpg" />
+        </ChangeImage>
+        <ChangeImage>
+          <p>anime</p>
+          <img className="anime" alt="anime" src="images/sManAll.jpg" />
+        </ChangeImage>
+        <ChangeImage>
+          <p>anime</p>
+          <img className="anime" alt="anime" src="images/bManAll.jpg" />
+        </ChangeImage>
+      </Carousel>
+    </div>
   );
 }
