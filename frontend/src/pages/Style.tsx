33683369893541aa7908499e6f90d65a06d93dea
Dong Seunghyeon <kj94gakt@naver.com>
2022-07-27 18:19:58 +0900

    chore: style code 수정

--- frontend/src/pages/Style.tsx
+++ frontend/src/pages/Style.tsx
@@ -1,5 +1,5 @@
-import React, { useState, Component } from 'react';
-import styled from 'styled-components';
+/* eslint-disable react/button-has-type */
+import React from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import 'react-responsive-carousel/lib/styles/carousel.css';
@@ -7,55 +7,6 @@ import '../components/carousel copy.css';
 import { Carousel } from 'react-responsive-carousel';
 import Header from '../components/Header';
 import BackgroundImage from '../components/BackgroundImage';
-
-const Sentence = styled.p`
-  width: 12rem;
-  height: 7.25rem;
-  font-family: Inter;
-  font-size: clamp(1.3rem, 10vw, 1rem);
-  font-weight: 500;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  color: #ffffff;
-`;
-
-const ChangeImage = styled.div`
-  width: 18.75rem;
-  height: 18.75rem;
-  margin: 0 auto;
-  padding: 4.563rem 2.5rem 1.063rem 3.125rem;
-  object-fit: contain;
-  border-radius: 100px;
-  text-align: center;
-  font-size: 1rem;
-
-  position: relative;
-`;
-
-const Btn = styled.button`
-  background-color: #9a9a9a;
-  width: 16.875rem;
-  height: 4rem;
-  border-radius: 50px;
-  border: 0;
-  outline: 0;
-  cursor: pointer;
-
-  margin: 2.063rem 1.625rem 0 0.063rem;
-  padding: 1.063rem 5.375rem 1.063rem 2.5rem;
-  justify-content: center;
-  align-items: center;
-`;
-
-const BtnContainer = styled.div`
-  position: absolute;
-  margin-left: 35%;
-  bottom: 15rem;
-  align-items: center;
-  justify-content: center;
-`;
 
 const handleOnClick = async (index: number, item: any) => {
   console.log(item.props.children.props.id);
@@ -143,30 +94,22 @@ export default function Style() {
             />
           </div>
         </Carousel>
-        {/* <button className="h-10 w-40 rounded-2xl bg-gray-400 text-white ml-4 ...">
-        <Link
-          to="/Pictureupload"
-          style={{ color: 'inherit', textDecoration: 'inherit' }}
-        >
-          사진 선택으로 돌아가기
-        </button>
-        <button className="h-10 w-40 rounded-2xl bg-gray-400 text-white ml-4 ..." onClick={() => console.log('선택 완료')}>
-          화풍선택
-        </button> */}
       </div>
-      <BtnContainer>
-        <Btn>
+      <div className="flex justify-center items-center">
+        <button className="h-10 w-40 rounded-2xl bg-gray-400 text-white ml-4 ...">
           <Link
             to="/Pictureupload"
             style={{ color: 'inherit', textDecoration: 'inherit' }}
-          >
-            <Sentence>사진 선택으로 돌아가기</Sentence>
-          </Link>
-        </Btn>
-        <Btn onClick={() => console.log('선택 완료')}>
-          <Sentence>화풍 선택</Sentence>
-        </Btn>
-      </BtnContainer>
+          />
+          사진 선택으로 돌아가기
+        </button>
+        <button
+          className="h-10 w-40 rounded-2xl bg-gray-400 text-white ml-4 ..."
+          onClick={() => console.log('선택 완료')}
+        >
+          화풍선택
+        </button>
+      </div>
     </BackgroundImage>
   );
 }
