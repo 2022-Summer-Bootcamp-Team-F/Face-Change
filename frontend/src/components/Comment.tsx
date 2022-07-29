4670507b568fb97f49c245a7a662fd0ed5a0a3fd
Dong Seunghyeon <kj94gakt@naver.com>
2022-07-29 13:27:51 +0900

    chore: tailwind로 수정

--- frontend/src/components/Comment.tsx
+++ frontend/src/components/Comment.tsx
@@ -1,47 +1,14 @@
-import React from "react";
-import styled from "styled-components";
-
-const Sentence1 = styled.p`
-  width: 31.125rem;
-  height: 4.813rem;
-  font-family: Inter;
-  font-size: 4rem;
-  font-weight: 500;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  color: #000;
-
-  margin: 0 auto;
-  text-align: center;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Sentence2 = styled.p`
-  width: 44.688rem;
-  height: 4.813rem;
-  font-family: Inter;
-  font-size: 4rem;
-  font-weight: 500;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  color: #000;
-
-  margin: 0 auto;
-  text-align: center;
-  justify-content: center;
-  align-items: center;
-`;
+import React from 'react';
 
 export default function Comment() {
   return (
-    <div className="Comment">
-      <Sentence1 className="sentence1">만화 속의 당신은</Sentence1>
-      <Sentence2 className="sentence2">어떤 모습인지 궁금한가요?</Sentence2>
+    <div>
+      <div className="flex w-[55rem] h-[4rem] m-auto justify-center align-items  font-bold text-6xl">
+        <p>만화 속의 당신은</p>
+      </div>
+      <div className="flex w-[55rem] h-[4rem] m-auto justify-center align-items mt-5 font-bold text-6xl">
+        <p>어떤 모습인지 궁금한가요?</p>
+      </div>
     </div>
   );
 }
