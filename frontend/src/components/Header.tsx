407afbd4d7d12f018f38f613f025813a15770f4a
Dong Seunghyeon <kj94gakt@naver.com>
2022-07-29 20:52:06 +0900

    chore: tailwind로 수정 및 로고 버튼 적용

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <div className="flex p-[50px]">
-      <img className="logoImage" alt="logo" src="images/logo.png" />
-      <img className="paletteImage" alt="palette" src="images/palette.png" />
+    <div className="Logo">
+      <a href="http://localhost:3000/">
+        <div className="flex p-[50px]">
+          <img className="logoImage" alt="logo" src="images/logo.png" />
+          <img
+            className="paletteImage"
+            alt="palette"
+            src="images/palette.png"
+          />
+        </div>
+      </a>
     </div>
   );
 }
