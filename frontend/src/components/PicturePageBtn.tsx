70c2558f117eb2047af8265f8baedc6bdea6c69d
Dong Seunghyeon <kj94gakt@naver.com>
2022-08-01 11:40:34 +0900

    chore: 코드 수정

--- frontend/src/components/PicturePageBtn.tsx
+++ frontend/src/components/PicturePageBtn.tsx
@@ -1,33 +1,6 @@
 /* eslint-disable react/button-has-type */
-import React from "react";
-import styled from "styled-components";
-import { Link } from "react-router-dom";
-
-const Btn = styled.button`
-  width: 150px;
-  height: 150px;
-  border-radius: 50%;
-  border: 1px solid black;
-  background-color: #4354f5;
-
-  top: 3.3rem;
-  margin: 0 auto;
-  padding-top: 1rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-
-  font-family: Inter;
-  font-size: 8rem;
-  font-weight: 100;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  text-align: left;
-  color: #fff;
-`;
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function PicturePageBtn() {
   return (
@@ -57,14 +30,14 @@ export default function PicturePageBtn() {
           </defs>
           <text dy="90" textLength="1220">
             <textPath xlinkHref="#textcircle_top">
-              사진 선택 + 사진 선택 + 사진 선택 + 사진 선택 +{" "}
+              사진 선택 + 사진 선택 + 사진 선택 + 사진 선택 +{' '}
             </textPath>
           </text>
         </svg>
         <button className="absolute flex justify-center items-center h-[9.5rem] w-[9.5rem] rounded-[50%] bg-blue-500 text-white text-4xl mr-[61rem] mb-[40rem] ...">
           <Link
             to="/Pictureupload"
-            style={{ color: "inherit", textDecoration: "inherit" }}
+            style={{ color: 'inherit', textDecoration: 'inherit' }}
           >
             +
           </Link>
