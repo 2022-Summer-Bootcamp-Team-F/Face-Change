f7e88fbdf6d187dcd9b026219a39ba6591d0de73
Dong Seunghyeon <kj94gakt@naver.com>
2022-08-05 15:01:49 +0900

    chore: Logo 수정

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,7 +5,12 @@ export default function Header() {
     <div className="Logo">
       <div className="flex p-[50px]">
         <a className="flex" href="http://localhost:3000/">
-          <img className="logoImage" alt="logo" src="images/logo.png" />
+          <img
+            className="logoImage"
+            alt="logo"
+            src="images/logo.png"
+            width="500"
+          />
           <img
             className="paletteImage"
             alt="palette"
