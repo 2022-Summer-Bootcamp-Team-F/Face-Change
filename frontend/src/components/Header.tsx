8fd15b2337b21be9e4c91947ed45a57db69633e7
Dong Seunghyeon <kj94gakt@naver.com>
2022-08-06 02:38:33 +0900

    style: header style 수정

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,11 +6,6 @@ export default function Header() {
       <div className="flex p-[50px] w-[30rem] h-[11.3rem]">
         <a className="flex" href="http://localhost:3000/">
           <img className="logoImage" alt="logo" src="images/logo.png" />
-          <img
-            className="paletteImage"
-            alt="palette"
-            src="images/palette.png"
-          />
         </a>
       </div>
     </div>
