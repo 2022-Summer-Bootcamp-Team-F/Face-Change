2da43e163bdbc6227ff6af4eec1c070a57941b2c
Dong Seunghyeon <kj94gakt@naver.com>
2022-08-06 03:40:39 +0900

    chore: header 코드 수정

--- frontend/src/components/ExamplePictures.tsx
+++ frontend/src/components/ExamplePictures.tsx
@@ -4,16 +4,16 @@ import './carousel2.css';
 import { Carousel } from 'react-responsive-carousel';
 
 const exampleList = [
-  { id: "1", key: "1", img: "images/All.jpg" },
+  { id: '1', key: '1', img: 'images/All.jpg' },
   {
-    id: "2",
-    key: "2",
-    img: "images/wman.jpg",
+    id: '2',
+    key: '2',
+    img: 'images/wman.jpg',
   },
-  { id: "3", key: "3", img: "images/bManAll.jpg" },
-  { id: "4", key: "4", img: "images/wgirl.jpg" },
-  { id: "5", key: "5", img: "images/rman.jpg" },
-  { id: "6", key: "6", img: "images/girlAll.jpg" },
+  { id: '3', key: '3', img: 'images/bManAll.jpg' },
+  { id: '4', key: '4', img: 'images/wgirl.jpg' },
+  { id: '5', key: '5', img: 'images/rman.jpg' },
+  { id: '6', key: '6', img: 'images/girlAll.jpg' },
 ];
 
 export default function ExamplePictures() {
