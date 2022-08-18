91659539d5f1e1660808ec2b05d487546dcde0d0
Choejeonghun <saa00123@naver.com>
2022-08-18 23:27:10 +0900

    feat: 반응형 작업 수정

--- frontend/src/components/Comment.tsx
+++ frontend/src/components/Comment.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
 export default function Comment() {
   return (
-    <div>
-      <div className="flex w-[55rem] h-[4rem] m-auto justify-center align-items  font-bold text-6xl">
+    <div className="font-bold xl:text-6xl lg:text-4xl md:text-2xl sm:text-1xl xl:w-[55rem] lg:w-[40rem] md:w-[30rem] sm:w-[20rem]">
+      <div className="flex m-auto justify-center align-items">
         <p>만화 속의 당신은</p>
       </div>
-      <div className="flex w-[55rem] h-[4rem] m-auto justify-center align-items mt-5 font-bold text-6xl">
+      <div className="flex m-auto justify-center align-items mt-5">
         <p>어떤 모습인지 궁금한가요?</p>
       </div>
     </div>
