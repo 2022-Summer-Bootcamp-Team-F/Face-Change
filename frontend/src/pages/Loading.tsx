8effe53bd29a1c1817be4515868cfce9beb75875
Choejeonghun <saa00123@naver.com>
2022-08-19 18:09:59 +0900

    feat: loadingpage 반응형

--- frontend/src/pages/Loading.tsx
+++ frontend/src/pages/Loading.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Header from "../components/Header";
 
 export default function Loading() {
@@ -8,18 +7,14 @@ export default function Loading() {
       className="absolute bg-center bg-cover w-full h-full"
       style={{ backgroundImage: "url(images/background.png)" }}
     >
-      <meta
-        httpEquiv="refresh"
-        content="5; url=http://localhost:3000/Results"
-      />
       <Header />
-      <div className="flex w-[16rem] h-[16rem] m-auto justify-center align-items mt-[5rem]">
+      <div className="flex xl:w-[16rem] lg:w-[12rem] md:w-[8rem] sm:w-[4rem] m-auto justify-center align-items mt-[5rem]">
         <img alt="spinner" src="images/Spinner.png" />
       </div>
-      <div className="flex w-[55rem] h-[4rem] m-auto justify-center align-items mt-[5rem] font-bold text-6xl">
+      <div className="flex xl:w-[55rem] lg:w-[40rem] md:w-[25rem] sm:w-[15rem] m-auto justify-center align-items mt-[5rem] font-bold xl:text-6xl lg:text-5xl md:text-4xl sm:text-2xl">
         당신을 위한 만화를 그리고 있습니다.
       </div>
-      <div className="flex w-[45rem] h-[4rem] m-auto justify-center align-items mt-[3rem] font-bold text-6xl">
+      <div className="flex xl:w-[55rem] lg:w-[40rem] md:w-[25rem] sm:w-[15rem] m-auto justify-center align-items mt-[3rem] font-bold xl:text-6xl lg:text-5xl md:text-4xl sm:text-2xl">
         잠시만 기다려 주세요...
       </div>
     </div>
