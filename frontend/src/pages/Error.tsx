1c77b46fb7bcc48af2ac5adc188ba0a633f842c1
Dong Seunghyeon <kj94gakt@naver.com>
2022-08-21 03:17:26 +0900

    feat: 반응형 작업 수정

--- frontend/src/pages/Error.tsx
+++ frontend/src/pages/Error.tsx
@@ -1,32 +1,37 @@
 /* eslint-disable react/style-prop-object */
 /* eslint-disable react/button-has-type */
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Error() {
   return (
     <div
       className="absolute bg-center bg-cover w-full h-full"
-      style={{ backgroundImage: "url(images/ErrorBackImg.png)" }}
+      style={{ backgroundImage: 'url(images/ErrorBackImg.png)' }}
     >
-      <div className="flex justify-center items-center m-auto mt-[3rem] w-[20rem] h-[20rem]">
+      <div className="flex justify-center items-center m-auto xl:w-[50rem] lg:w-[40rem] md:w-[30rem] sm:w-[20rem] mt-[1rem]">
         <img className="ErrorIcon" alt="erroricon" src="images/ErrorIcon.png" />
       </div>
-      <div className="flex justify-center items-center text-5xl mt-10 font-bold">
-        <p>오류</p>
+      <div className="font-bold m-auto xl:text-5xl lg:text-4xl md:text-2xl sm:text-1xl xl:w-[100rem] lg:w-[40rem] md:w-[30rem] sm:w-[20rem]">
+        <div className="flex m-auto mt-2 justify-center items-center">
+          <p>오류</p>
+        </div>
       </div>
-      <div className="flex justify-center items-center text-center text-5xl mt-[3rem] font-bold">
-        <p>
-          만화 그리기에 실패했습니다.
-          <br />
-          주의 사항을 읽어보시고 다시 사진을 업로드해주세요.
-        </p>
+      <div className="font-bold m-auto xl:text-5xl lg:text-4xl md:text-2xl sm:text-1xl xl:w-[100rem] lg:w-[40rem] md:w-[30rem] sm:w-[20rem]">
+        <div className="flex m-auto mt-7 justify-center items-center">
+          <p>만화 그리기에 실패했습니다.</p>
+        </div>
+      </div>
+      <div className="font-bold m-auto xl:text-5xl lg:text-4xl md:text-2xl sm:text-1xl xl:w-[100rem] lg:w-[40rem] md:w-[30rem] sm:w-[20rem]">
+        <div className="flex m-auto justify-center items-center">
+          <p>주의 사항을 읽어보시고 다시 사진을 업로드해주세요.</p>
+        </div>
       </div>
       <div className="flex justify-center items-center mt-20">
-        <button className="h-[4rem] w-[17rem] rounded-full rounded-4xl bg-blue-500 text-white text-2xl ml-4 ...">
+        <button className="h-[4rem] xl:w-[17rem] lg:w-[15rem] md:w-[12rem] sm:w-[10rem] rounded-full rounded-4xl bg-blue-500 text-white xl:text-2xl lg:text-1.5xl md:text-1xl sm:text=0.5xl ml-4 ...">
           <Link
             to="/Pictureupload"
-            style={{ color: "inherit", textDecoration: "inherit" }}
+            style={{ color: 'inherit', textDecoration: 'inherit' }}
           >
             사진 재 선택
           </Link>
