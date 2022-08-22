a88c61aff2f2bbb384c0355f14e0dc7ff9ed27f9
Choejeonghun <saa00123@naver.com>
2022-08-22 22:44:11 +0900

    style: uploadpage 반응형 수정

--- frontend/src/pages/Pictureupload.tsx
+++ frontend/src/pages/Pictureupload.tsx
@@ -32,11 +32,11 @@ function Pictureupload() {
       style={{ backgroundImage: "url(images/background.png)" }}
     >
       <Header />
-      <div className="flex justify-center items-center xl:ml-[26.5rem] md:ml-[15rem] sm:ml-[15rem]">
-        <div className="flex justify-center items-center border-dotted xl:w-[30rem] md:w-[27rem] sm:w-[30rem] xl:h-[30rem] md:h-[25rem] sm:h-[20rem] p-4 border-4">
+      <div className="flex justify-center items-center xl:ml-[26.5rem] md:ml-[15rem] sm:ml-[12rem] sml:ml-[7rem] xl:mt-0 md:mt-[3rem] sm:mt-[5rem] sml:mt-[8rem]">
+        <div className="flex justify-center items-center border-dotted xl:w-[30rem] md:w-[27rem] sm:w-[50rem] sml:w-[80rem] xl:h-[30rem] md:h-[25rem] sm:h-[20rem] sml:h-[20rem] p-4 border-4">
           {fileIma && <img alt="sample" id="sample" src={fileIma} />}
         </div>
-        <div className="flex justify-center items-center border-dotted xl:w-[15rem] md:w-[10rem] xl:h-[20rem] md:h-[15rem] p-4 border-4 xl:ml-[11rem] md:ml-[5rem]">
+        <div className="flex justify-center items-center border-dotted xl:w-[15rem] md:w-[10rem] xl:h-[20rem] md:h-[15rem] xl:visible md:visible sm:invisible sml:invisible p-4 border-4 xl:ml-[11rem] md:ml-[5rem]">
           <p>
             <p className="text-center">주의 사항</p>
             <br />
@@ -46,9 +46,9 @@ function Pictureupload() {
           </p>
         </div>
       </div>
-      <div className="flex justify-center items-center mt-[3rem] xl:text-[1rem] sm:text-[0.7rem]">
+      <div className="flex justify-center items-center mt-[3rem] text-[1rem]">
         <label
-          className="flex justify-center items-center xl:w-40 sm:w-20 h-10 rounded-2xl bg-blue-500 text-white"
+          className="flex justify-center items-center xl:w-40 sml:w-[7rem] h-10 rounded-2xl bg-blue-500 text-white"
           htmlFor="name"
         >
           사진 업로드
@@ -62,7 +62,7 @@ function Pictureupload() {
           />
         </label>
         {visible && (
-          <button className="xl:w-40 sm:w-20 h-10 rounded-2xl bg-blue-500 text-white ml-4">
+          <button className="xl:w-40 sml:w-[7rem] h-10 rounded-2xl bg-blue-500 text-white ml-4">
             <Link
               to={{ pathname: "/Style" }}
               state={{ fileImage }}
@@ -73,7 +73,7 @@ function Pictureupload() {
           </button>
         )}
         <button
-          className="xl:w-40 sm:w-20 h-10 rounded-2xl bg-red-500 text-white ml-4"
+          className="xl:w-40 sml:w-[7rem] h-10 rounded-2xl bg-red-500 text-white ml-4"
           onClick={() => deleteFileImage()}
         >
           삭제
