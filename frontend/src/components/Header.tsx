832ce886e9562d2dabf820cfe94c14be8d4abee7
Choejeonghun <saa00123@naver.com>
2022-08-22 22:31:25 +0900

    style: header sml 사이즈 추가

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <div className="Logo">
-      <div className="flex p-[50px] xl:w-[30rem] lg:w-[30rem] md:w-[25rem] sm:w-[20rem] mt-[1rem]">
+      <div className="flex p-[50px] xl:w-[30rem] md:w-[25rem] sm:w-[20rem] sml:w-[20rem] mt-[1rem]">
         <a className="flex" href="http://localhost:3000/">
           <img className="logoImage" alt="logo" src="images/logo.png" />
         </a>
